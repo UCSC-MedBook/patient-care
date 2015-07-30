1897dce4ce6b1cca764993048f6d0b5fa2faf7bb
mokolodi1 <mokolodi1@gmail.com>
2015-07-29 19:31:06 -0700

    made a boxplot in d3

--- d3playground/trying.js
+++ d3playground/trying.js
@@ -1,7 +1,8 @@
 document.onreadystatechange = function () { // make sure DOM is there
   if (document.readyState == "complete") {
 
-    var theData = [
+    // global
+    theData = [
       { "value" : -1.7744098012610501 },
       { "value" : -1.6705581809928001 },
       { "value" : 2.6623082962344098 },
@@ -53,73 +54,249 @@ document.onreadystatechange = function () { // make sure DOM is there
       { "value" : -1.6400219953537600 },
       { "value" : -2.1896467439921299 },
       { "value" : 0.7688794601192210 },
-      { "value" : 0.6416002682141340 }
+      { "value" : 0.6416002682141340 },
+
+      { "value" : -9 },
+      { "value" : 9 },
+
+      { "value" : -6.1 },
+      { "value" : -6.2 },
+
+      //{ "value" : 6},
+      { "value" : 6.1},
     ];
 
-    var axisContext = {
-      "height": 20,
+    var theContext = {
+      "height": 60,
       "width": 200,
-      "minimum_value": -3,
-      "maximum_value": 3,
+      "minimum_value": -10,
+      "maximum_value": 10,
+      "lower_threshold_value": -3,
+      "higher_threshold_value": .6,
     }
 
-    initAxis(theData, axisContext);
-    initChart();
+    initChart(theData, theContext);
   }
 };
 
-function initAxis(theData, context) {
-  console.log("initAxis function");
+function initChart(theData, context) {
+  console.log('initChart function');
 
-  // make sure context has height, width
-  if (!context) {
-    console.log("Cannot render boxplot: context missing");
-  } else if (!context.height || !context.width) {
-    console.log("Cannot render boxplot: context missing height and/or width");
+  //
+  // set up variables, default values
+  //
+
+  var margin;
+  if (context.margin === undefined) {
+    margin = {};
+  } else {
+    margin = context.margin;
   }
+  _.defaults(margin, {top: 0, right: 8, bottom: 0, left: 8});
 
-  // set up default values
-  var margin = context.margin
-      || {top: 0, right: 5, bottom: 0, left: 5};
+  // note: context must have height and width
   var width = context.width - margin.left - margin.right;
   var height = context.height - margin.top - margin.bottom;
 
-  theData = theData.sort(function (a, b) { // make sure the data are sorted
-    return b.value - a.value;
-  })
+  // make sure the data are sorted (for pulling min/max values)
+  theData = theData.sort(function (a, b) {
+    return a.value - b.value;
+  });
 
+  // pull min/max from context if possible
   var minimumValue = context.minimum_value
-                      || theData[theData.length - 1].value;
+      || theData[theData.length - 1].value;
   var maximumValue = context.maximum_value
-                      || theData[0].value;
+      || theData[0].value;
 
   var xScale = d3.scale.linear() // converts data values to pixel values
-      .domain([maximumValue, minimumValue])
+      .domain([minimumValue, maximumValue])
       .range([0, width]);
 
-  var xAxis = d3.svg.axis()
-      .scale(xScale)
-      .orient("top") // the data is below
-      .ticks(width / 35); // how many ticks there are
-
-  var svg = d3.select("#the-axis-id").append("svg")
+  var svg = d3.select("#the-chart-id")
+      .append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .selectAll("text")
-      .attr("y", -10) // where the text is in relation to the tick
-      .attr("x", 0)
-      .style("text-anchor", "middle");
-}
+  //
+  // helper and drawing helper functions
+  //
 
-function initChart() {
-  console.log('initChart function');
+  function attachAxis(axisHeight) {
+    console.log("attachAxis function");
 
-  
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient("top") // the data is below
+        .ticks(width / 35); // how many ticks there are
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + axisHeight + ")")
+        .call(xAxis)
+      .selectAll("text")
+        .attr("y", -10) // where the text is in relation to the tick
+        .attr("x", 0)
+        .style("text-anchor", "middle");
+  }
+
+  function valueAtPercent(array, percent) {
+    // percent = [0, 1]
+    // valueAtPercent([1, 2, 3, 4, 5], .25) ==> 2
+    // valueAtPercent([1, 2, 3, 4, 5], .20) ==> 1.8
+    var index = (array.length - 1) * percent;
+    if (index === Math.floor(index)) { // return immidiately if int
+      return array[index].value;
+    }
+
+    var lowerIndex = Math.floor(index);
+    var higherIndex = Math.ceil(index);
+    var distanceToLower = index - lowerIndex;
+    var distanceToHigher = higherIndex - index;
+
+    return array[lowerIndex].value * distanceToHigher
+        + array[higherIndex].value * distanceToLower;
+  }
+
+  function getSurroundingIndexes(array, needleValue) {
+    // https://github.com/Olical/binary-search/blob/master/src/binarySearch.js
+    var min = 0;
+    var max = array.length - 1;
+    var guess;
+
+    while (min <= max) {
+      guess = Math.floor((min + max) / 2);
+
+      if (array[guess].value === needleValue) {
+        return [guess, guess];
+      } else {
+        if (array[guess].value < needleValue) {
+          min = guess + 1;
+        } else {
+          max = guess - 1;
+        }
+      }
+    }
+
+    if (min === 0) {
+      return [min, min];
+    }
+    if (max === array.length - 1) {
+      return [max, max];
+    }
+
+    // there are outliers
+    if (array[min].value < needleValue) {
+      return [min, min + 1];
+    } else {
+      return [min - 1, min];
+    }
+  }
+
+  // what it looks like
+  var axisHeight = 20;
+  var boxHeight = 20;
+  var boxMiddle = 20; // line drawn here
+
+  //
+  // start drawing things
+  //
+
+  attachAxis(axisHeight);
+
+  var boxplot = svg.append("g")
+      .attr("class", "boxplot")
+      .attr("transform", "translate(0," + axisHeight + ")")
+      .selectAll("bleh") // doesn't work without this (no selection)
+
+  var firstQuartile = valueAtPercent(theData, .25);
+  var secondQuartile = valueAtPercent(theData, .5);
+  var thirdQuartile = valueAtPercent(theData, .75);
+  var interquartileRange = thirdQuartile - firstQuartile;
+
+  console.log("firstQuartile - 1.5 * interquartileRange: ", firstQuartile - 1.5 * interquartileRange);
+  console.log("thirdQuartile + 1.5 * interquartileRange: ", thirdQuartile + 1.5 * interquartileRange);
+
+  var leftWhiskerValue = theData[
+        getSurroundingIndexes(theData , firstQuartile
+            - 1.5 * interquartileRange)[1]
+      ].value;
+  var rightWhiskerValue = theData[
+        getSurroundingIndexes(theData , thirdQuartile
+            + 1.5 * interquartileRange)[0]
+      ].value;
+
+  function significanceClass(firstValue, secondValue) {
+    var value = (firstValue + secondValue) / 2;
+
+    if (value < context.lower_threshold_value
+        || value > context.higher_threshold_value) {
+      return "outside-threshold";
+    }
+    return "inside-threshold";
+  }
+
+  // vertical whisker lines
+  boxplot.data([leftWhiskerValue, rightWhiskerValue])
+      .enter()
+      .append("line")
+      .attr("x1", xScale)
+      .attr("x2", xScale)
+      .attr("y1", boxMiddle - boxHeight / 2)
+      .attr("y2", boxMiddle + boxHeight / 2)
+      .attr("class", function (value, index) {
+        return significanceClass(value, value);
+      });
+
+  // horizontal lines
+  function drawLine(firstValue, secondValue) {
+    if (firstValue < secondValue) {
+      boxplot.data([0]).enter()
+          .append("line")
+          .attr("x1", xScale(firstValue))
+          .attr("x2", xScale(secondValue))
+          .attr("y1", boxMiddle)
+          .attr("y2", boxMiddle)
+          .attr("class", significanceClass(firstValue, secondValue));
+    }
+  }
+  drawLine(leftWhiskerValue, context.lower_threshold_value); // left
+  drawLine(Math.max(context.lower_threshold_value, leftWhiskerValue)
+      , Math.min(context.higher_threshold_value, rightWhiskerValue)); // middle
+  drawLine(context.higher_threshold_value, rightWhiskerValue); // right
+
+  // boxes
+  function drawBox(firstValue, secondValue) {
+    if (firstValue < secondValue) {
+      boxplot.data([0]).enter()
+          .append("rect")
+          .attr("x", xScale(firstValue))
+          .attr("y", boxMiddle - boxHeight / 2)
+          .attr("height", boxHeight)
+          .attr("width", xScale(secondValue) - xScale(firstValue))
+          .attr("class", significanceClass(firstValue, secondValue));
+    }
+  }
+  drawBox(firstQuartile, context.lower_threshold_value); // left blue
+  drawBox(Math.max(context.lower_threshold_value, firstQuartile)
+      , Math.min(context.higher_threshold_value, thirdQuartile)); // grey
+  drawBox(context.higher_threshold_value, thirdQuartile); // right blue
+
+  // outliers
+  boxplot.data(_.filter(theData, function (current) {
+        return current.value < leftWhiskerValue
+            || current.value > rightWhiskerValue;
+      }))
+      .enter()
+      .append("circle")
+      .attr("cx", function (object, index) {
+        return xScale(object.value);
+      })
+      .attr("cy", boxMiddle)
+      .attr("r", 2)
+      .attr("class", function (object, index) {
+        return significanceClass(object.value, object.value);
+      });
 }
