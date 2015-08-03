dbc27031ea019ded42db85798c748a3590046590
mokolodi1 <mokolodi1@gmail.com>
2015-08-02 21:51:05 -0700

    ready for Monday

--- webapp/client/templates/patientReport/patientReport.js
+++ webapp/client/templates/patientReport/patientReport.js
@@ -22,7 +22,7 @@ Template.renderChart.helpers({
   removeSpaces: removeSpaces,
 });
 
-Session.setDefault("chartToRender", "boxAndWhisker");
+Session.setDefault("chartToRender", "waterfall");
 
 Template.renderChart.rendered = function () {
   // must have the data ready to be called :)
@@ -35,12 +35,12 @@ Template.renderChart.rendered = function () {
     // one specific to that chart type, one specific to that chart
     var context = {
       "chart_type": Session.get("chartToRender"),
-      "height": 50,
+      "height": Session.get("chartToRender") === "waterfall" ? 100 : 50,
       "width": 150,
       "minimum_value": -10,
       "maximum_value": 10,
-      "lower_threshold_value": -1.5,//data.lower_threshold_value,
-      "upper_threshold_value": 1.5,//data.upper_threshold_value,
+      //"lower_threshold_value": -1.5,//data.lower_threshold_value,
+      //"upper_threshold_value": 1.5,//data.upper_threshold_value,
       "dom_selector": data.type + data.algorithm
           + removeSpaces(data.label),
       "highlighted_sample_labels":
@@ -54,5 +54,27 @@ Template.renderChart.rendered = function () {
 
     Charts.render(data['sample_values'], context);
   });
-
 };
+
+// who would ever put inline css in the database?
+
+function saveTheText(offendingString, beginLength, endLength) {
+  var firstCut = offendingString
+      //.replace(/\n|\r/g, "")
+      .slice(beginLength);
+  return firstCut.substr(0, firstCut.length - endLength);
+}
+
+Template.showTreatment.helpers({
+  trimTedsDomElementInMongo: function (theEvidence) {
+    var firstPart;
+    if (theEvidence.includes("Prior")) {
+      firstPart = "<div style='width:300px;'><a href='/CRF/lists/SU2C_Prior_TX_V3/7snEyTx8m2hzSWzJn/'>";
+    } else {
+      firstPart = "<div style='width:300px;'><a href='/CRF/lists/SU2C_Subsequent_Treatment_V1/Toa2hxBxyZshCMtNS/'>";
+    }
+    var secondPart = "</a></div>";
+
+    return saveTheText(theEvidence, firstPart.length, secondPart.length);
+  },
+});
