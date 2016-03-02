d6d90d16a739bddf037e4725881b10c493797314
chrisw <chrisw@soe.ucsc.edu>
2016-03-01 20:44:44 -0800

    use medbook:circlemap-graph@0.3.0

--- webapp/server/publications.js
+++ webapp/server/publications.js
@@ -1,36 +1,33 @@
-Meteor.publish("PatientReport", function (patientLabel) {
-  return PatientReports.find(
-        // TODO: sort by date (once there are multiple per patient)
-        {"patient_label": patientLabel},
-        {limit: 1}
-      );
+Meteor.publish("PatientReport", function(patientLabel) {
+    return PatientReports.find(
+    // TODO: sort by date (once there are multiple per patient)
+    {
+        "patient_label" : patientLabel
+    }, {
+        limit : 1
+    });
 });
 
 function rankCohortSignature(cohortSignature, sampleLabels) {
-  var samples = cohortSignature.samples;
-  for (var labelIndex = sampleLabels.length - 1;
-      labelIndex >= 0;
-      labelIndex--) {
-    var label = sampleLabels[labelIndex];
-    for (var index in samples) {
-      if (label === samples[index].sample_label) {
-        return labelIndex + index / samples.length;
-      }
+    var samples = cohortSignature.samples;
+    for (var labelIndex = sampleLabels.length - 1; labelIndex >= 0; labelIndex--) {
+        var label = sampleLabels[labelIndex];
+        for (var index in samples) {
+            if (label === samples[index].sample_label) {
+                return labelIndex + index / samples.length;
+            }
+        }
     }
-  }
-  //console.log("ERROR: samples not found in given provided cohort signature");
-  return 0;
+    //console.log("ERROR: samples not found in given provided cohort signature");
+    return 0;
 }
 
 function sortByRank(first, second) {
-  return second.rank - first.rank;
+    return second.rank - first.rank;
 }
 
-Meteor.publish("topCohortSignatures",
-  function(typeName, maxCount, sampleLabels) {
-    console.log("publishing topCohortSignatures  typeName:", typeName,
-        "  maxCount:", maxCount,
-        "  sampleLabels:", sampleLabels);
+Meteor.publish("topCohortSignatures", function(typeName, maxCount, sampleLabels) {
+    console.log("publishing topCohortSignatures  typeName:", typeName, "  maxCount:", maxCount, "  sampleLabels:", sampleLabels);
 
     check(typeName, String);
     check(maxCount, Number);
@@ -41,110 +38,72 @@ Meteor.publish("topCohortSignatures",
     var lowestPublishedRank = -1;
 
     function addTopOnes() {
-      var allOfType = CohortSignatures.find({
-        "type": typeName,
-        "samples": {
-          $elemMatch: {
-            sample_label: {
-              $in: sampleLabels
+        var allOfType = CohortSignatures.find({
+            "type" : typeName,
+            "samples" : {
+                $elemMatch : {
+                    sample_label : {
+                        $in : sampleLabels
+                    }
+                }
             }
-          }
-        }
-      }).fetch();
+        }).fetch();
 
-      if (allOfType.length > 0) {
-        // assign ranks
-        for (var index in allOfType) {
-          allOfType[index].rank = rankCohortSignature(allOfType[index],
-              sampleLabels);
-        }
+        if (allOfType.length > 0) {
+            // assign ranks
+            for (var index in allOfType) {
+                allOfType[index].rank = rankCohortSignature(allOfType[index], sampleLabels);
+            }
 
-        var toPublish = allOfType.sort(sortByRank).slice(0, maxCount);
-        lowestPublishedRank = toPublish[toPublish.length - 1].rank;
+            var toPublish = allOfType.sort(sortByRank).slice(0, maxCount);
+            lowestPublishedRank = toPublish[toPublish.length - 1].rank;
 
-        // publish top ones according to rank
-        for (var current in toPublish) {
-          self.added("cohort_signatures", toPublish[current]._id,
-              toPublish[current]);
+            // publish top ones according to rank
+            for (var current in toPublish) {
+                self.added("cohort_signatures", toPublish[current]._id, toPublish[current]);
+            }
         }
-      }
     }
 
-    var handle = CohortSignatures.find({"type": typeName}).observeChanges({
-      added: function (id, newOne) {
-        if (!initializing) {
-          console.log("something was added to CohortSignatures:", newOne);
-          if (lowestPublishedRank < rankCohortSignature(newOne, sampleLabels)) {
-            // TODO: should we remove the old ones?
-            self.added("cohort_signatures", id, newOne);
-          }
+    var handle = CohortSignatures.find({
+        "type" : typeName
+    }).observeChanges({
+        added : function(id, newOne) {
+            if (!initializing) {
+                console.log("something was added to CohortSignatures:", newOne);
+                if (lowestPublishedRank < rankCohortSignature(newOne, sampleLabels)) {
+                    // TODO: should we remove the old ones?
+                    self.added("cohort_signatures", id, newOne);
+                }
+            }
+        },
+        removed : function(id) {
+            console.log("something was removed in CohortSignatures");
+        },
+        changed : function(id) {
+            console.log("something changed in CohortSignatures");
         }
-      },
-      removed: function (id) {
-        console.log("something was removed in CohortSignatures");
-      },
-      changed: function (id) {
-        console.log("something changed in CohortSignatures");
-      }
     });
 
     initializing = false;
     addTopOnes();
 
     // Stop observing the cursor when client unsubs.
-    self.onStop(function () {
-      handle.stop();
+    self.onStop(function() {
+        handle.stop();
     });
 
     self.ready();
-  }
-);
-
-var getTargettingDrugs = function(genes){
-	var ignoreDrugs = ["N/A", ""];
-	var cursor = ClinicalEvidenceSummaries.find({"gene": { $in: genes },"evidence_direction":"Supports","clinical_significance":"Sensitivity", "drugs": {$nin: ignoreDrugs}},
-	{fields:{gene:1,drugs:1,pubmed_id:1}});
-	return cursor;
-};
-
-Meteor.publish("GeneReport", function (geneLabel) {
-  var geneReportCursor = GeneReports.find(
-    {"gene_label": geneLabel},
-    {limit: 1}
-  );
-  var currentReport = geneReportCursor.fetch()[0];
-
-  if (currentReport) { // in case we don't have one
-    var geneNames = _.pluck(currentReport.network.elements, 'name');
-    geneNames = _.uniq(geneNames);
-    // TODO get targetting drugs
-    var targettingDrugsCursor = getTargettingDrugs(geneNames);
-    var expression2Cursor = Expression2.find({"gene": { $in: geneNames }});
-    var cohortSignaturesCursor = CohortSignatures.find({
-      "algorithm": "viper",
-      "label": { $in: geneNames },
-    });
-
-    return [
-      geneReportCursor,
-      expression2Cursor,
-      cohortSignaturesCursor,
-      targettingDrugsCursor
-    ];
-  } else {
-    // must return this for Meteor to say the subscription is ready
-    return [];
-  }
 });
 
 // allows quick linking between patient reports
-Meteor.publish("ReportMetadata", function () {
-	var count = PatientReports.find({}).count();
-  console.log("publishing report metadata. PatientReports count=",count);
-  return PatientReports.find({}, {
-    fields: {
-      "patient_label": 1,
-      "metadata": 1,
-    }
-  });
+Meteor.publish("ReportMetadata", function() {
+    var count = PatientReports.find({}).count();
+    console.log("publishing report metadata. PatientReports count=", count);
+    return PatientReports.find({}, {
+        fields : {
+            "patient_label" : 1,
+            "metadata" : 1,
+        }
+    });
 });
