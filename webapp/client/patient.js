5e2490c4f328d687f5c633d588b2e8e0c49898c1
Teo Fleming <mokolodi1@gmail.com>
2016-04-26 02:10:06 -0700

    can create sample groups on the fly

--- webapp/client/patient.js
+++ webapp/client/patient.js
@@ -128,15 +128,12 @@ Template.patientUpDownGenes.onCreated(function () {
 
   instance.sampleLabel = new ReactiveVar();
   instance.sampleGroupId = new ReactiveVar();
-  instance.customSampleGroup = new ReactiveVar(false);
+  // whether they want to create a custom sample group
+  instance.createCustomSampleGroup = new ReactiveVar(false);
+  // the custom sample group data (initialized if needed in sub-template)
+  instance.customSampleGroup = new ReactiveVar();
   instance.error = new ReactiveVar(); // { header: "Uh oh", message: "hi" }
   instance.waitingForResponse = new ReactiveVar(false);
-});
-
-Template.patientUpDownGenes.helpers({
-  getSampleGroups: function () {
-    return SampleGroups.find({});
-  },
 });
 
 Template.patientUpDownGenes.onRendered(function () {
@@ -145,17 +142,26 @@ Template.patientUpDownGenes.onRendered(function () {
   instance.$(".ui.dropdown").dropdown();
 });
 
+Template.patientUpDownGenes.helpers({
+  getSampleGroups: function () {
+    return SampleGroups.find({});
+  },
+  getCustomSampleGroup: function () {
+    return Template.instance().customSampleGroup; // return ReactiveVar
+  },
+});
+
 Template.patientUpDownGenes.events({
   "click .choose-sample-label": function (event, instance) {
     instance.sampleLabel.set(this.valueOf());
   },
   "click .choose-custom-sample-group": function (event, instance) {
-    instance.customSampleGroup.set(true);
+    instance.createCustomSampleGroup.set(true);
     instance.sampleGroupId.set(null);
   },
   "click .choose-sample-group": function (event, instance) {
     instance.sampleGroupId.set(this._id);
-    instance.customSampleGroup.set(false);
+    instance.createCustomSampleGroup.set(false);
   },
   "click .close-error-message": function (event, instance) {
     instance.error.set(null);
@@ -167,8 +173,6 @@ Template.patientUpDownGenes.events({
     instance.error.set(null);
 
     let sample_label = instance.sampleLabel.get();
-    let sample_group_id = instance.sampleGroupId.get();
-
     if (!sample_label) {
       instance.error.set({
         header: "Uh oh!",
@@ -177,35 +181,360 @@ Template.patientUpDownGenes.events({
       return;
     }
 
-    if (!sample_group_id) {
-      instance.error.set({
-        header: "What about the background?",
-        message: "Please select a background sample group above to continue."
+    // sometimes we have to make two trips to the server, so we need to be
+    // able to call this from async code (after a Meteor method has been run)
+    function createUpDownGenes (sample_group_id) {
+      let args = _.pick(instance.data, "study_label", "patient_label");
+      _.extend(args, { sample_label, sample_group_id });
+
+      // submit to the server for consideration
+      Meteor.call("createUpDownGenes", args, function (error, job_id) {
+        if (error) {
+          instance.error.set({
+            header: "Internal error",
+            message: "We had a problem processing your request... If this " +
+                "messages persists, please " + contactTeoText,
+          });
+        } else {
+          FlowRouter.go("upDownGenes", _.extend(args, { job_id }));
+        }
+
+        instance.waitingForResponse.set(false);
       });
+    }
+
+    if (instance.createCustomSampleGroup.get()) {
+      let customSampleGroup = instance.customSampleGroup.get();
+
+      // sanity checks
+      if (!customSampleGroup.name) {
+        instance.error.set({
+          header: "Whoops!",
+          message: "Please name your sample group."
+        });
+        return;
+      }
+      if (customSampleGroup.studies.length === 0) {
+        instance.error.set({
+          header: "No studies?",
+          message: "Please add at least one study to your sample group."
+        });
+        return;
+      }
+
+      instance.waitingForResponse.set(true);
+      Meteor.call("createSampleGroup", customSampleGroup, (error, result) => {
+        if (error) {
+          instance.waitingForResponse.set(false);
+
+          instance.error.set({
+            header: "Internal error",
+            message: "We had a problem creating a sample group... If this " +
+                "messages persists, please " + contactTeoText,
+          });
+        } else {
+          createUpDownGenes(result);
+        }
+      });
+    } else {
+      let sampleGroupId = instance.sampleGroupId.get();
+
+      if (!sampleGroupId) {
+        instance.error.set({
+          header: "Did you forget something?",
+          message: "Please select a background sample group to continue."
+        });
+        return;
+      }
+
+      instance.waitingForResponse.set(true);
+      createUpDownGenes(sampleGroupId)
+    }
+  },
+});
+
+
+
+// Template.editSampleGroup
+
+Template.editSampleGroup.onCreated(function () {
+  let instance = this;
+
+  // load all available studies
+  instance.subscribe("studies");
+
+  instance.sampleGroup = instance.data.sampleGroup;
+  if (!instance.sampleGroup.get()) { // make sure it's initialized
+    let samples = Studies.findOne({id: "prad_wcdt"}).gene_expression;
+    let filtered = samples.slice(0, 10);
+
+    instance.sampleGroup.set({
+      name: "",
+      version: 1,
+      collaborations: [ Meteor.user().collaborations.personal ],
+      studies: [
+        // {
+        //   study_label: "prad_wcdt",
+        //   filters: [
+        //     {
+        //       type: "sample_label_list",
+        //       options: {
+        //         sample_labels: [ "DTB-001", "DTB-002" ]
+        //       }
+        //     }
+        //   ]
+        // }
+      ]
+    });
+  }
+
+  // store this seperately so that we don't look for a version every time
+  instance.name = new ReactiveVar("");
+
+  // look up the name in the sample groups this person has access to
+  // and figure out what version this one should be
+  let updateVersion = _.debounce((name) => {
+    Meteor.call("getSampleGroupVersion", name, (error, result) => {
+      let sampleGroup = instance.sampleGroup.get();
+      sampleGroup.version = result;
+      instance.sampleGroup.set(sampleGroup);
+    });
+  }, 250);
+  instance.autorun(() => {
+    updateVersion(instance.name.get());
+  });
+
+  // update the sample group with the name
+  instance.autorun(() => {
+    // don't rerun when sample group changes
+    let sampleGroup = Tracker.nonreactive(() => {
+      return instance.sampleGroup.get();
+    });
+    sampleGroup.name = instance.name.get();
+    instance.sampleGroup.set(sampleGroup);
+  });
+});
+
+Template.editSampleGroup.onRendered(function () {
+  let instance = this;
+
+  instance.$(".sample-group-version").popup({
+    position : "top right",
+  });
+});
+
+Template.editSampleGroup.helpers({
+  sampleGroup: function () {
+    return Template.instance().sampleGroup; // returns ReactiveVar
+  },
+  getSampleGroup: function () {
+    return Template.instance().sampleGroup.get();
+  },
+  addableStudies: function () {
+    let existingStudies = Template.instance().sampleGroup.get().studies;
+
+    // only return studies that haven't already been added
+    return Studies.find({
+      id: { $nin: _.pluck(existingStudies, "study_label") },
+    });
+  },
+  getStudyName: function (study_label) {
+    let study = Studies.findOne({id: study_label});
+    if (study) {
+      return study.name;
+    } else {
+      return "You don't have access to this study.";
+    }
+  },
+});
+
+Template.editSampleGroup.events({
+  "keyup .sample-group-name": function (event, instance) {
+    instance.name.set(event.target.value);
+  },
+  "click .remove-study": function (event, instance) {
+    let sampleGroup = instance.sampleGroup.get();
+
+    sampleGroup.studies = _.filter(sampleGroup.studies, (study) => {
+      return study.study_label !== this.study_label
+    });
+
+    instance.sampleGroup.set(sampleGroup);
+  },
+});
+
+
+
+// Template.addStudyMenu
+
+Template.addStudyMenu.onRendered(function () {
+  let instance = this;
+
+  instance.$(".dropdown").popup({
+    hoverable: true,
+    on: "click",
+  });
+});
+
+Template.addStudyMenu.events({
+  "click .add-study-to-sample-group": function (event, instance) {
+    let sampleGroup = instance.data.sampleGroup.get();
+
+    sampleGroup.studies.push({
+      study_label: this.id,
+      filters: [],
+    });
+
+    instance.data.sampleGroup.set(sampleGroup);
+  },
+});
+
+
+
+// Template.addFilterButton
+
+Template.addFilterButton.onRendered(function () {
+  let instance = this;
+
+  instance.$(".dropdown").popup({
+    hoverable: true,
+    on: "click",
+  });
+});
+
+Template.addFilterButton.events({
+  "click .add-sample-label-list-filter": function (event, instance) {
+    // the popup moves down weirdly, so hide it
+    instance.$(".dropdown").popup("hide");
+
+    let sampleGroup = instance.data.sampleGroup.get();
+
+    sampleGroup.studies[instance.data.studyIndex].filters.push({
+      type: "sample_label_list",
+      options: {
+        sample_labels: []
+      },
+    });
+
+    instance.data.sampleGroup.set(sampleGroup);
+  },
+});
+
+
+
+
+// Template.showFilter
+
+Template.showFilter.onCreated(function () {
+  let instance = this;
+
+  instance.sampleGroup = instance.data.sampleGroup;
+  instance.editing = new ReactiveVar(false);
+
+  instance.setOptions = function (newOptions) {
+    let sampleGroup = instance.sampleGroup.get();
+
+    let { filterIndex, studyIndex } = instance.data;
+    sampleGroup.studies[studyIndex].filters[filterIndex].options = newOptions;
+
+    instance.sampleGroup.set(sampleGroup);
+  };
+});
+
+Template.showFilter.helpers({
+  getFilter: function () {
+    let { sampleGroup, data } = Template.instance();
+
+    let study = sampleGroup.get().studies[data.studyIndex];
+    if (study) { // remove error on remove study
+      return study.filters[data.filterIndex];
+    }
+  },
+  setOptions: function () {
+    return Template.instance().setOptions;
+  },
+  editing: function () {
+    return Template.instance().editing; // returns ReactiveVar
+  },
+  getEditing: function () {
+    return Template.instance().editing.get();
+  },
+  study_label: function () {
+    let instance = Template.instance();
+    return instance.sampleGroup.get()
+        .studies[instance.data.studyIndex].study_label;
+  },
+});
+
+Template.showFilter.events({
+  "click .edit-filter-toggle": function (event, instance) {
+    instance.editing.set(!instance.editing.get());
+  },
+  "click .remove-filter": function (event, instance) {
+    let sampleGroup = instance.sampleGroup.get();
+
+    let { filterIndex, studyIndex } = instance.data;
+    sampleGroup.studies[studyIndex].filters.splice(filterIndex, 1);
+
+    instance.sampleGroup.set(sampleGroup);
+  },
+});
+
+
+
+// Template.sampleLabelListFilter
+
+Template.sampleLabelListFilter.onCreated(function () {
+  let instance = this;
+
+  instance.invalidSampleLabels = new ReactiveVar(null);
+});
+
+Template.sampleLabelListFilter.helpers({
+  sampleLabelsToText: function () {
+    return this.options.sample_labels.join("\n");
+  },
+  getInvalidSampleLabels: function () {
+    return Template.instance().invalidSampleLabels.get();
+  },
+});
+
+Template.sampleLabelListFilter.events({
+  "click .done-editing": function (event, instance) {
+    // clear errors
+    instance.invalidSampleLabels.set(null);
+
+    // let's gooo (split by whitespace characters, get rid of spaces)
+    let textareaSampleLabels = instance.$("textarea").val().split(/[\s,]+/);
+    let sample_labels = _.filter(textareaSampleLabels, (value) => {
+      return value; // remove falsey
+    });
+
+    // make sure we don't have any bad values
+    let study = Studies.findOne({id: instance.data.study_label});
+    let studySampleLabelMap = _.reduce(study.Sample_IDs, (memo, label) => {
+      memo[label] = true;
+      return memo;
+    }, {});
+
+    let badValues = _.filter(sample_labels, (label) => {
+      return !studySampleLabelMap[label];
+    });
+
+    if (badValues.length) {
+      instance.invalidSampleLabels.set(badValues);
       return;
     }
 
-    // submit to the server for consideration
-    instance.waitingForResponse.set(true);
-    let args = _.pick(instance.data, "study_label", "patient_label");
-    _.extend(args, { sample_label, sample_group_id });
-
-    Meteor.call("createUpDownGenes", args, function (error, job_id) {
-      instance.waitingForResponse.set(false);
-
-      if (error) {
-        console.log("error:", error);
-        instance.error.set({
-          header: "Internal error",
-          message: "We had a problem processing your request... If this " +
-              "messages persists, please " + contactTeoText,
-        });
-      } else {
-
-        FlowRouter.go("upDownGenes", _.extend(args, { job_id }));
-      }
+    // nicely done! set the options and return to non-editing
+    instance.data.setOptions({
+      sample_labels
     });
-
+    instance.data.editing.set(false);
+  },
+  "click .close-sample-error-message": function (event, instance) {
+    console.log("click");
+    instance.invalidSampleLabels.set(null);
   },
 });
 
--- webapp/lib/methods.js
+++ webapp/lib/methods.js
@@ -18,6 +18,11 @@ Meteor.methods({
       sample_group_name: sampleGroup.name,
     });
 
+    // make sure they have access to each of the studies in the sample group
+    _.each(sampleGroup.studies, (sampleGroupStudy) => {
+      user.ensureAccess(Studies.findOne({id: sampleGroupStudy.study_label}));
+    });
+
     // check to see if a job like this one has already been run,
     // and if so, return that job's _id
     let duplicateJob = Jobs.findOne({ args });
@@ -33,5 +38,86 @@ Meteor.methods({
       user_id: user._id,
       args
     });
+  },
+  getSampleGroupVersion: function (name) {
+    // return the next version given the sample group name
+    // NOTE: this function only looks at the sample groups this user has
+    // access to, which means sample groups are not necessarily uniquely
+    // identifiable by { name, version }.
+
+    let user = MedBook.ensureUser(Meteor.userId());
+
+    let latestSampleGroup = SampleGroups.findOne({
+      name,
+      collaborations: { $in: user.getCollaborations() },
+    }, { sort: { version: -1 } });
+
+    if (latestSampleGroup) {
+      return latestSampleGroup.version + 1
+    }
+
+    return 1; // default value
+  },
+  createSampleGroup: function (sampleGroup) {
+    check(sampleGroup, Object); // more checking down below...
+
+    // NOTE: this method might produce "unclean" errors because I don't
+    // feel like rewriting most of the schema for SampleGroups for the
+    // check function (above)
+
+    let user = MedBook.findUser(Meteor.userId());
+    user.ensureAccess(sampleGroup);
+
+    // make sure the version is correct (aka don't trust the user)
+    sampleGroup.version =
+        Meteor.call("getSampleGroupVersion", sampleGroup.name);
+
+    // ensure uniqueness for studies
+    let uniqueStudies = _.uniq(_.pluck(sampleGroup.studies, "study_label"));
+    if (uniqueStudies.length !== sampleGroup.studies.length) {
+      throw new Meteor.Error("non-unique-studies");
+    }
+
+    // filter through each study
+    // - make sure they have access
+    // - filter the samples
+    sampleGroup.studies = _.map(sampleGroup.studies, (sampleGroupStudy) => {
+      let study = Studies.findOne({id: sampleGroupStudy.study_label});
+      user.ensureAccess(study);
+
+      // start with all the samples and then filter down from there
+      let sample_labels = study.Sample_IDs;
+
+      _.each(sampleGroupStudy.filters, (filter) => {
+        if (filter.type === "sample_label_list") {
+          // for good measure, make sure all the sample labels are in the array
+          let studySampleLabelMap =
+              _.reduce(study.Sample_IDs, (memo, label) => {
+            memo[label] = true;
+            return memo;
+          }, {});
+
+          let badValues = _.filter(filter.options.sample_labels, (label) => {
+            return !studySampleLabelMap[label];
+          });
+
+          if (badValues.length) {
+            throw new Meteor.Error("invalid-sample-label-list");
+          }
+
+          // actually do the filtering
+          sample_labels =
+              _.intersection(sample_labels, filter.options.sample_labels);
+        } else {
+          throw new Meteor.Error("invalid-filter-type");
+        }
+      });
+
+      sampleGroupStudy.sample_labels = sample_labels;
+
+      return sampleGroupStudy; // NOTE: _.map at beginning
+    });
+
+    return SampleGroups.insert(sampleGroup)
   },
 });
