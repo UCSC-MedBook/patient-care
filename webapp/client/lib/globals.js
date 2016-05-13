0a322d521c556e8fa9d61a472916dd9bec100606
Teo Fleming <mokolodi1@gmail.com>
2016-05-13 05:47:12 +0000

    improved error messages

--- webapp/client/lib/globals.js
+++ webapp/client/lib/globals.js
@@ -1,5 +1,5 @@
 contactTeoText =
-`<a href="mailto:mokolodi1@gmail.com?subject=PatientCare%20bug" class="ui button teal">
+`<a href="mailto:dtflemin@ucsc.edu?subject=PatientCare%20bug" class="ui button teal">
   <i class="bug icon"></i>
   Contact Teo
 </a>`;
